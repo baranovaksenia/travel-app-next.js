0161b87f433b18f040fa442719076fd51de3c741
Ksenia Baranova <baranova-ksenia@mail.ru>
2022-02-21 12:39:06 +0200

    Feat(footer): Create footer

---
+++ app/components/common/footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import styles from './Footer.module.scss'
+
+const navItems = [
+  {
+    icon: 'home',
+    link: '/',
+  },
+  {
+    icon: 'explore',
+    link: '/explore',
+  },
+  {
+    icon: 'place',
+    link: '/place',
+  },
+  {
+    icon: 'person_outline',
+    link: '/profile',
+  },
+]
+const Footer = () => {
+  const { push, pathname } = useRouter()
+  return (
+    <footer className={styles.footer}>
+      <nav>
+        {navItems.map((item) => (
+          <button
+            className={pathname === item.link ? styles.active : ''}
+            onClick={() => push(item.link)}
+            key={item.icon}
+          >
+            <span className='material-icons-outlined'>{item.icon}</span>
+          </button>
+        ))}
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
